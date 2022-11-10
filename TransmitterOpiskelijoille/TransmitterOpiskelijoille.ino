94264ab5fba901e4befb29f45db317e793258503
JuhoLaurila00 <juho.laurila00@gmail.com>
2022-11-10 11:09:10 +0200

    asento laitetaan flagiin

--- TransmitterOpiskelijoille/TransmitterOpiskelijoille.ino
+++ TransmitterOpiskelijoille/TransmitterOpiskelijoille.ino
@@ -27,6 +27,34 @@ void loop()
      
      uint8_t id = M;
      uint8_t flags = 0xff;
+
+     //Kysytään missä asennossa arduino on
+     Serial.println("Give position of arduino (left=0, right=1, up=2, down=3)");
+     int pos = 0;
+     while(pos==0)
+     {
+        if(Serial.available()>0)
+          {
+            pos = Serial.parseInt();
+          }
+     }
+     //Määritetään hexa arvo flagiin asennon mukaan
+     switch (pos){
+        case 0:
+          flags = 0x00;
+        break;
+        case 1:
+          flags = 0x01;
+        break;
+        case 2:
+          flags = 0x02;
+        break;
+        case 3:
+          flags = 0x03;
+        break;
+      }
+     
+      
      Mobject.createMessage(m);
      if(Mobject.sendMessage(id,flags))
      {
