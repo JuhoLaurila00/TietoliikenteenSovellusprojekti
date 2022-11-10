96a2e71c4afb4cf455791c96424eae2298334dc3
JuhoLaurila00 <juho.laurila00@gmail.com>
2022-11-10 11:18:21 +0200

    Korjaus

--- TransmitterOpiskelijoille/TransmitterOpiskelijoille.ino
+++ TransmitterOpiskelijoille/TransmitterOpiskelijoille.ino
@@ -20,40 +20,41 @@ void loop()
     }
   }
 
+  uint8_t flags = 0xff;
+  
+  //Kysytään missä asennossa arduino on
+  Serial.println("Give position of arduino (left=1, right=2, up=3, down=4)");
+  int pos = 0;
+  while(pos==0)
+  {
+    if(Serial.available()>0)
+    {
+      pos = Serial.parseInt();
+    }
+  }
+   //Määritetään hexa arvo flagiin asennon mukaan
+   switch (pos){
+    case 1:
+      flags = 0x01;
+      break;
+    case 2:
+      flags = 0x02;
+      break;
+    case 3:
+      flags = 0x03;
+      break;
+    case 4:
+      flags = 0x04;
+      break;
+   }
+
+  
   for(int M = 0;M<NumberOfMeasurements;M++)
   {
      Aobject.makeMeasurement();
      Measurement m = Aobject.getMeasurement();
      
      uint8_t id = M;
-     uint8_t flags = 0xff;
-
-     //Kysytään missä asennossa arduino on
-     Serial.println("Give position of arduino (left=0, right=1, up=2, down=3)");
-     int pos = 0;
-     while(pos==0)
-     {
-        if(Serial.available()>0)
-          {
-            pos = Serial.parseInt();
-          }
-     }
-     //Määritetään hexa arvo flagiin asennon mukaan
-     switch (pos){
-        case 0:
-          flags = 0x00;
-        break;
-        case 1:
-          flags = 0x01;
-        break;
-        case 2:
-          flags = 0x02;
-        break;
-        case 3:
-          flags = 0x03;
-        break;
-      }
-     
       
      Mobject.createMessage(m);
      if(Mobject.sendMessage(id,flags))
