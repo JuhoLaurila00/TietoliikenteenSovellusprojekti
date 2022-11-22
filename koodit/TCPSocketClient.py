32fcbdc6d2944b8e3c8c29db4b47715e8f2e88f7
JuhoLaurila00 <juho.laurila00@gmail.com>
2022-11-22 17:50:06 +0200

    TCPClient valmis

---
+++ koodit/TCPSocketClient.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server_address = ('172.20.241.9', 20000)
+client_socket.connect(server_address)
+
+try:
+    client_socket.sendall('59\n'.encode())
+except socket.error as e: 
+    print ("Socket error: %s" %str(e)) 
+except Exception as e: 
+    print ("Other exception: %s" %str(e)) 
+
+reply = bytearray()
+n = 15000
+while len(reply) < n:
+    packet = client_socket.recv(n - len(reply))
+    if not packet: 
+        break
+    reply.extend(packet)
+
+
+client_socket.close() 
+
+data = reply.decode()
+print(data)
+
+
+with open('data.csv', 'w') as f:
+    f.write(data) 
