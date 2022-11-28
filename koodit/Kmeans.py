4ab8d2601865d6cf27ce207abb1423bc4c750e36
JuhoLaurila00 <juho.laurila00@gmail.com>
2022-11-28 10:59:09 +0200

    ...

--- koodit/Kmeans.py
+++ koodit/Kmeans.py
@@ -6,3 +6,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
 data = np.loadtxt('putty.log')
+
+nData = np.zeros((int(data.shape[0] / 3), 3))
+
+i = 0
+tracker = 0
+while i < nData.shape[0]:
+    nData[i][0] = data[tracker]
+    nData[i][1] = data[tracker+1]
+    nData[i][2] = data[tracker+2]
+    i+=1
+    tracker+=3
+    
+print(nData)
